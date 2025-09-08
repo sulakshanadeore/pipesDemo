0bd4bad1dde0f46d3335047c85f1927c64853837
sai <sai_prasad_veluri@yahoo.com>
2025-09-08 06:58:36 +0000

    password pattern matching demo

--- src/app/status-demo/status-demo.component.ts
+++ src/app/status-demo/status-demo.component.ts
@@ -8,8 +8,30 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class StatusDemoComponent {
 demoForm=new FormGroup({
-    username:new FormControl('',[Validators.required,Validators.minLength(3)])
+    username:new FormControl('',[Validators.required,Validators.minLength(3)]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8),
+    Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)
+      
+    ])
   });
+
+  get password()
+  {
+    return this.demoForm.get('password');
+  }
+
+
+  OnSubmit()
+  {
+    if(this.password.valid)
+    {
+      alert("Valid");
+    }
+    else
+    {
+      alert("Invalid");
+    }
+  }
 
   get username()
   {
